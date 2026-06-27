7b131493daf14b19ff038f07c6704a6a39da3b10
Putra <airbender840@gmail.com>
2026-06-28 02:07:45 +0700

    Update Homebrew formula checksum

--- packaging/homebrew/opti-mac.rb
+++ packaging/homebrew/opti-mac.rb
@@ -2,7 +2,7 @@ class OptiMac < Formula
   desc "Safe macOS cleanup and maintenance CLI"
   homepage "https://github.com/agungdp150/optimac"
   url "https://github.com/agungdp150/optimac/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_SHA256"
+  sha256 "42789287818626a584a34565d7eecaa9dd1de7f24282f8cfbbd1ab7c14e18ff4"
   license "MIT"
 
   depends_on "go" => :build
